fa87578fe9935dc2e8117944589ae750e30d83fb
narcis <narcissprecic@gmail.com>
2022-01-20 09:44:52 -0500

    api

--- src/services/RoverApiService.ts
+++ src/services/RoverApiService.ts
@@ -2,10 +2,11 @@ import axios from "axios";
 import { Photos } from "../models/mars-rover";
 
 export function fetchRoverPhotos(): Promise<Photos[]>{
+    const apiKey = process.env.REACT_APP_API_KEY;
     return axios.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos", {
         params:{
             sol: 1000,
-            api_key: "wUWAmPHaTLaWxa95exYPiXAtn0u325gX9O0YRmBG"
+            api_key: apiKey
         }
     })
     .then(res=>res.data.photos)
